09182a7cecf6f940d036e9560d9087105a41ccf4
MangKyu <whalsrb1226@gmail.com>
2022-09-15 20:37:58 +0900

    Initial commit

---
+++ src/main/java/com/mangkyu/openfeign/OpenFeignSampleApplication.java
@@ -0,0 +1,13 @@
+package com.mangkyu.openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeignSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignSampleApplication.class, args);
+    }
+
+}
