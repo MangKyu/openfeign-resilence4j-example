4281ae2086d1b082c6a698f932168b4b0d624312
MangKyu <whalsrb1226@gmail.com>
2022-10-05 19:14:04 +0900

    Add Retryer Config

--- src/main/java/com/mangkyu/openfeign/config/OpenFeignConfig.java
+++ src/main/java/com/mangkyu/openfeign/config/OpenFeignConfig.java
@@ -10,9 +10,12 @@
 package com.mangkyu.openfeign.config;
 
 import feign.Logger;
+import feign.Retryer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableFeignClients("com.mangkyu.openfeign")
@@ -21,5 +24,11 @@ class OpenFeignConfig {
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    Retryer.Default retryer() {
+        // 0.1초의 간격으로 시작해 최대 3초의 간격으로 점점 증가하며, 최대5번 재시도한다.
+        return new Retryer.Default(100L, TimeUnit.SECONDS.toMillis(3L), 5);
     }
 }
