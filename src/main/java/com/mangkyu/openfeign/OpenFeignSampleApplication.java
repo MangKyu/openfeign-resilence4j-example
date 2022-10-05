e81337aad6dc9ccd6681a46e03f3e684b4a093bb
MangKyu <whalsrb1226@gmail.com>
2022-10-05 19:13:44 +0900

    Add WebMvcTest for Open Feign Config location

--- src/main/java/com/mangkyu/openfeign/OpenFeignSampleApplication.java
+++ src/main/java/com/mangkyu/openfeign/OpenFeignSampleApplication.java
@@ -2,7 +2,9 @@ package com.mangkyu.openfeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//@EnableFeignClients
 @SpringBootApplication
 public class OpenFeignSampleApplication {
 
