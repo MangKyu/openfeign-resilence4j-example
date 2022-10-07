26581ad6e0d27bde3b582a43fec2e9f9c5a69f00
MangKyu <whalsrb1226@gmail.com>
2022-10-07 20:11:16 +0900

    Update FeignTestContext

--- src/main/java/com/mangkyu/openfeign/config/OpenFeignConfig.java
+++ src/main/java/com/mangkyu/openfeign/config/OpenFeignConfig.java
@@ -29,7 +29,7 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableFeignClients("com.mangkyu.openfeign")
-class OpenFeignConfig {
+public class OpenFeignConfig {
 
     @Bean
     Logger.Level feignLoggerLevel() {
--- src/test/java/com/mangkyu/openfeign/feigntest/FeignTestContext.java
+++ src/test/java/com/mangkyu/openfeign/feigntest/FeignTestContext.java
@@ -1,19 +1,19 @@
 package com.mangkyu.openfeign.feigntest;
 
 import com.mangkyu.openfeign.config.CustomPropertiesConfig;
+import com.mangkyu.openfeign.config.OpenFeignConfig;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
 import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-@EnableFeignClients(basePackages = "com.mangkyu.openfeign")
 @ImportAutoConfiguration({
+        OpenFeignConfig.class,
         CustomPropertiesConfig.class,
         FeignAutoConfiguration.class,
         HttpMessageConvertersAutoConfiguration.class,
         ConfigurationPropertiesAutoConfiguration.class
 })
 class FeignTestContext {
-    
+
 }
