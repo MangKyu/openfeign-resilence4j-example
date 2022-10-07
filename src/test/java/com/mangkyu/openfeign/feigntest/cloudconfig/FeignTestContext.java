c7e81d22a66c68d24a9dccadf20d2c0aa454d4ed
MangKyu <whalsrb1226@gmail.com>
2022-10-07 19:45:10 +0900

    Add TestConfig for Feign

--- src/test/java/com/mangkyu/openfeign/feigntest/cloudconfig/FeignTestContext.java
+++ src/test/java/com/mangkyu/openfeign/feigntest/cloudconfig/FeignTestContext.java
@@ -1,15 +1,18 @@
 package com.mangkyu.openfeign.feigntest.cloudconfig;
 
+import com.mangkyu.openfeign.config.CustomPropertiesConfig;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
 import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @EnableFeignClients(basePackages = "com.mangkyu.openfeign")
 @ImportAutoConfiguration({
-        FeignAutoConfiguration.class,
+		CustomPropertiesConfig.class,
+		FeignAutoConfiguration.class,
         HttpMessageConvertersAutoConfiguration.class,
-		FeignContextPropertiesConfig.class,
+		ConfigurationPropertiesAutoConfiguration.class
 })
 class FeignTestContext {
 }
