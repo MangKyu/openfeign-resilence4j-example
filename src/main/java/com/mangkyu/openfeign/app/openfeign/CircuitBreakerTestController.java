6f084dd238ce28cd1247b0d1434274574f65ef69
MangKyu <whalsrb1226@gmail.com>
2023-02-13 17:48:44 +0900

    Refactor

--- src/main/java/com/mangkyu/openfeign/app/openfeign/CircuitBreakerTestController.java
+++ src/main/java/com/mangkyu/openfeign/app/openfeign/CircuitBreakerTestController.java
@@ -13,18 +13,23 @@ import com.mangkyu.openfeign.app.Currency;
 import com.mangkyu.openfeign.app.ExchangeRateResponse;
 import feign.FeignException;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.vavr.collection.Seq;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.circuitbreaker.NoFallbackAvailableException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 class CircuitBreakerTestController {
 
     private final ExchangeRateOpenFeign feign;
@@ -43,16 +48,32 @@ class CircuitBreakerTestController {
     }
 
     @GetMapping("/circuit/close")
-    public ResponseEntity<Void> close() {
-        circuitBreakerRegistry.circuitBreaker("default")
+    public ResponseEntity<Void> close(@RequestParam String name) {
+        circuitBreakerRegistry.circuitBreaker(name)
                 .transitionToClosedState();
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/circuit/open")
-    public ResponseEntity<Void> open() {
-        circuitBreakerRegistry.circuitBreaker("default")
+    public ResponseEntity<Void> open(@RequestParam String name) {
+        circuitBreakerRegistry.circuitBreaker(name)
                 .transitionToOpenState();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/circuit/status")
+    public ResponseEntity<CircuitBreaker.State> status(@RequestParam String name) {
+        CircuitBreaker.State state = circuitBreakerRegistry.circuitBreaker(name)
+                .getState();
+        return ResponseEntity.ok(state);
+    }
+
+    @GetMapping("/circuit/all")
+    public ResponseEntity<Void> all() {
+        Seq<CircuitBreaker> circuitBreakers = circuitBreakerRegistry.getAllCircuitBreakers();
+        for (CircuitBreaker circuitBreaker : circuitBreakers) {
+            log.error("circuitName={}, state={}", circuitBreaker.getName(), circuitBreaker.getState());
+        }
         return ResponseEntity.ok().build();
     }
 
@@ -62,11 +83,11 @@ class CircuitBreakerTestController {
                 .body(Collections.singletonMap("code", "FeignException"));
     }
 
-//    @ExceptionHandler(NoFallbackAvailableException.class)
-//    public ResponseEntity<?> handleNoFallbackAvailableException(NoFallbackAvailableException e) {
-//        return ResponseEntity.badRequest()
-//                .body(Collections.singletonMap("code", "NoFallbackAvailableException"));
-//    }
+    @ExceptionHandler(NoFallbackAvailableException.class)
+    public ResponseEntity<?> handleNoFallbackAvailableException(NoFallbackAvailableException e) {
+        return ResponseEntity.badRequest()
+                .body(Collections.singletonMap("code", "NoFallbackAvailableException"));
+    }
 
     @ExceptionHandler(CallNotPermittedException.class)
     public ResponseEntity<?> handleCallNotPermittedException(CallNotPermittedException e) {
